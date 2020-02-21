42c9b1064c829761e14571be7de80d024bc4ded9
Ilkka Mäkinen <ilkka.makinen@suuntastudios.com>
2020-02-21 11:09:37 +0100

    injektointien lämmittely

---
+++ viikko14-16/RiippuvuuksienInjektointi1/src/main/java/ohtu/IO.java
@@ -0,0 +1,9 @@
+package ohtu;
+
+/**
+ * IO
+ */
+public interface IO {
+    int nextInt();
+    void print(String m);
+}
---
+++ viikko14-16/RiippuvuuksienInjektointi1/src/main/java/ohtu/KonsoliIO.java
@@ -0,0 +1,22 @@
+package ohtu;
+
+import java.util.Scanner;
+
+/**
+ * KonsoliIO
+ */
+public class KonsoliIO implements IO {
+    private Scanner lukija;
+    
+    public KonsoliIO() {
+        lukija = new Scanner(System.in);
+    }
+
+    public int nextInt() {
+        return lukija.nextInt();
+    }
+
+    public void print(String m) {
+        System.out.println(m);
+    }
+}
