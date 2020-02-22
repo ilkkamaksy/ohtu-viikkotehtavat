c98ce9e7a15d04251d9804d74e6930659d3e8463
Ilkka Mäkinen <ilkka.makinen@suuntastudios.com>
2020-02-22 13:07:48 +0100

    tehtävät 1 ja 2

---
+++ viikko2/gradle-tehtava/src/main/java/ohtu/Multiplier.java
@@ -0,0 +1,18 @@
+package ohtu;
+
+/**
+ * Multiplier
+ */
+public class Multiplier {
+    
+    private int value;
+    
+    public Multiplier(int value) {
+        this.value = value;
+    }
+
+    public int multipliedBy(int other) {
+        return value * other;
+    }
+    
+}
